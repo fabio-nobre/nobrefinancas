bffabe35dcfa50f8f15d13ca76bf1ee8b140f78b
Fábio Nobre <f.oliveiranobre@gmail.com>
2026-03-06 16:06:43 -0300

    dashboard com signals local

--- src/app/features/dashboard/pages/dashboard-page.component.ts
+++ src/app/features/dashboard/pages/dashboard-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core'
+import { Component, inject, computed } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FinanceiroStore } from '@/app/application/financeiro/stores/financeiro.store'
 import { SaldoCardComponent } from '../components/saldo-card/saldo-card.component'
 import { UltimosLancamentosComponent } from '../components/ultimos-lancamentos/ultimos-lancamentos.component'
 import { GastosCategoriaChartComponent } from '../components/gastos-categoria-chart/gastos-categoria-chart.component'
 import { EvolucaoMensalChartComponent } from '../components/evolucao-mensal-chart/evolucao-mensal-chart.component'
+
 
 @Component({
   selector: 'app-dashboard-page',
@@ -27,32 +28,32 @@ Dashboard Financeiro
 
 <app-saldo-card
 titulo="Saldo atual"
-[valor]="store.saldo()"
+[valor]="saldo()"
 tipo="saldo">
 </app-saldo-card>
 
 <app-saldo-card
 titulo="Receitas"
-[valor]="store.totalReceitas()"
+[valor]="totalReceitas()"
 tipo="receita">
 </app-saldo-card>
 
 <app-saldo-card
 titulo="Despesas"
-[valor]="store.totalDespesas()"
+[valor]="totalDespesas()"
 tipo="despesa">
 </app-saldo-card>
 
 <app-saldo-card
 titulo="Saldo previsto"
-[valor]="store.saldoPrevisto()"
+[valor]="saldoPrevisto()"
 tipo="saldo">
 </app-saldo-card>
 
 </div>
 
 <app-evolucao-mensal-chart
-  [lancamentos]="store.lancamentos()">
+  [lancamentos]="lancamentos()">
 </app-evolucao-mensal-chart>
 
 <!-- 📊 gráfico -->
@@ -64,7 +65,7 @@ Gastos por categoria
 </div>
 
 <app-gastos-categoria-chart
-[lancamentos]="store.lancamentos()">
+[lancamentos]="lancamentos()">
 </app-gastos-categoria-chart>
 
 </div>
@@ -72,7 +73,7 @@ Gastos por categoria
 <!-- 📋 lançamentos -->
 
 <app-ultimos-lancamentos
-[lancamentos]="store.ultimosLancamentos()">
+[lancamentos]="ultimosLancamentos()">
 </app-ultimos-lancamentos>
 
 </div>
@@ -81,5 +82,29 @@ Gastos por categoria
 export class DashboardPageComponent {
 
   store = inject(FinanceiroStore)
+
+  lancamentos = computed(() =>
+    this.store.lancamentos()
+  )
+
+  ultimosLancamentos = computed(() =>
+    this.store.ultimosLancamentos()
+  )
+
+  saldoPrevisto = computed(() =>
+    this.store.saldoPrevisto()
+  )
+
+  saldo = computed(() =>
+    this.store.saldoPrevisto()
+  )
+
+  totalReceitas = computed(() =>
+    this.store.saldoPrevisto()
+  )
+
+  totalDespesas = computed(() =>
+    this.store.saldoPrevisto()
+  )
 
 }
